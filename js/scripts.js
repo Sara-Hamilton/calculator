bb4c8c58085ffc520e22864070e628ecaaf125ce
Frank Ngo and Sara Hamilton <sara@>
2018-01-08 11:56:05 -0800

    add bmi function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -25,11 +25,20 @@
 // var result = multiply(number1, number2);
 // alert(result);
 
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = divide(number1, number2);
+// alert(result);
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+var bmi = function(height, weight) {
+  return ((weight/(height*height)*703));
+}
+
+var height = parseInt(prompt("What is your height in inches?"));
+var weight = parseInt(prompt("What is your weight in pounds?"));
+var result = bmi(height, weight);
+alert("Your BMI is " + result.toPrecision(4));
