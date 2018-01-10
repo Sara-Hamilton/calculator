5a630ed2a01a954232a9a0df18c45023d8b684be
Hannah Lee and Sara Hamilton <hannah@>
2018-01-10 11:40:20 -0800

    separate business logic and user interface logic, and add jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,49 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
 // var result = add(number1, number2);
 // alert(result);
 
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// };
-//
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
 // var result = subtract(number1, number2);
 // alert(result);
 
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// };
-//
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
 // var result = multiply(number1, number2);
 // alert(result);
 
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
-//
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
 // var result = divide(number1, number2);
 // alert(result);
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
 
 // var bmi = function(height, weight) {
 //   return ((weight/(height*height)*703));
@@ -43,11 +54,11 @@
 // var result = bmi(height, weight);
 // alert("Your BMI is " + result.toPrecision(4));
 
-var temp_convert = function(celsius){
-  return (celsius * 1.8) + 32;
-};
-
-var celsius = prompt("Enter your tempurature in celsius");
-var result = temp_convert(celsius);
-
-alert(result + " degrees fahrenheit");
+// var temp_convert = function(celsius){
+//   return (celsius * 1.8) + 32;
+// };
+//
+// var celsius = prompt("Enter your tempurature in celsius");
+// var result = temp_convert(celsius);
+//
+// alert(result + " degrees fahrenheit");
